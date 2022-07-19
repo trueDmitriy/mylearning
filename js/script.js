38aac443bfbde817e83464da0c635ce5e4938548
rong <true.dimitriy@gmail.com>
2022-07-19 13:03:49 +0300

    first

---
+++ js/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+
+const answers = [];
+answers[0] = prompt('Your name?', '');
+answers[1] = prompt('Your secondname?', '');
+answers[2] = prompt('Your are?', '');
+
+
+console.log(typeof(answers));
