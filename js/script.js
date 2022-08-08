ab868cb2184ce4eeb77438e6a9f935901681e956
rong <true.dimitriy@gmail.com>
2022-08-08 17:24:43 +0300

    second

--- js/script.js
+++ js/script.js
@@ -1,10 +1,38 @@
 "use strict";
 
+/*var numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+console.log(numberOfFilms);
 
-const answers = [];
-answers[0] = prompt('Your name?', '');
-answers[1] = prompt('Your secondname?', '');
-answers[2] = prompt('Your are?', '');
+var personalMovieDB ={
+  'count': numberOfFilms,
+  'movies':{},
+  'actors':{},
+  'genres':[],
+  'privat':false
+};
+
+var a = prompt('Один из последних просмотренных фильмов?', '');
+var b = prompt('На сколько оцените его?', '');
+var c = prompt('Один из последних просмотренных фильмов?');
+var d = prompt('На сколько оцените его?');
 
 
-console.log(typeof(answers));
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB); */
+var numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+var personalMovieDB ={
+  'count': numberOfFilms,
+  'movies':{},
+  'actors':{},
+  'genres':[],
+  'privat':false
+};
+
+for ( let i = 0 ; i < 2 ; i++){
+  let FilmName = prompt('Один из последних просмотренных фильмов?', '');
+  let FilmRating = prompt('На сколько оцените его?', '');
+  personalMovieDB.movies[FilmName] = FilmRating;
+}
+console.log(personalMovieDB);
